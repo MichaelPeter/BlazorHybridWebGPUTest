7fc550efc79f61141d9b6d653ebc4f141e348ad0
MichaelPeter <michaelpeteronline@googlemail.com>
2021-12-26 03:24:42 +0100

    Add Browser version add flgas

--- WpfClient/MainWindow.xaml.cs
+++ WpfClient/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Web.WebView2.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,18 @@ namespace BlazorHybridWebGPUTest.WpfClient
             Resources.Add("services", serviceCollection.BuildServiceProvider());
 
             InitializeComponent();
+            //SetWebViewEnviromentAsync().Wait(); // Find better was than to wait.
+        }
+
+        private async Task SetWebViewEnviromentAsync()
+        {
+            var envOptions = new CoreWebView2EnvironmentOptions(additionalBrowserArguments: "--enable-features=unsafe-webgpu"); // Check link regarding spelling
+            var env = await CoreWebView2Environment.CreateAsync(options: envOptions);
+
+            // Make shure settings are set: // TODO: Better method than wait.
+            // https://stackoverflow.com/questions/58604537/how-to-change-microsoft-edge-webview2-mdns-behavior-by-calling-createwebview2env
+            // https://stackoverflow.com/questions/67728100/webview2-additionalbrowserarguments-kiosk-printing
+            await BlazorWebView.WebView.EnsureCoreWebView2Async(env);
         }
 
         private void OnExitClick(object sender, RoutedEventArgs e)
@@ -38,6 +51,16 @@ namespace BlazorHybridWebGPUTest.WpfClient
         private void OnOpenDeveloperConsole(object sender, RoutedEventArgs e)
         {
             BlazorWebView.WebView.CoreWebView2.OpenDevToolsWindow();
+        }
+
+        public void OnShowBrowserVersionString(object sender, RoutedEventArgs e)
+        {
+            string versionString = BlazorWebView.WebView.CoreWebView2.Environment.BrowserVersionString;
+
+
+            // To specify minimum version: BlazorWebView.WebView.CoreWebView2.Environment.CompareBrowserVersions
+
+            MessageBox.Show($"Browser Version: {versionString}", "Browser Version Info", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         
